bac093ff82e61ac814d3fe8cfc9fbaee0b3c1ecd
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-05-05 15:45:09 +0430

    learned making menus

--- app/src/main/java/io/github/muhammadkarbalaee/myapplication/MainActivity.java
+++ app/src/main/java/io/github/muhammadkarbalaee/myapplication/MainActivity.java
@@ -2,20 +2,37 @@ package io.github.muhammadkarbalaee.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    this.setContentView(R.layout.activity_main);
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.main,menu);
+    this.getMenuInflater().inflate(R.menu.test_menu,menu);
+    return true;
+  }
+
+  @SuppressLint("NonConstantResourceId")
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    String text = "default";
+    switch (item.getItemId()) {
+      case R.id.about: text = "about";
+        break;
+      case R.id.settings: text = "settings";
+        break;
+    }
+    Toast.makeText(this,text,Toast.LENGTH_LONG).show();
     return true;
   }
 }
