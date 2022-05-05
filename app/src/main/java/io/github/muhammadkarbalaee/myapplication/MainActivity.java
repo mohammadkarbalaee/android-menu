b7fe86585320a396ea780ff26a16114703b80f30
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2022-05-05 15:14:58 +0430

    initialized

---
+++ app/src/main/java/io/github/muhammadkarbalaee/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package io.github.muhammadkarbalaee.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.main,menu);
+    return true;
+  }
+}
